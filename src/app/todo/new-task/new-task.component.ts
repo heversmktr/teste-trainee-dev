5633576a7588cdfab53865be28d5598061251147
Heverson Morais <heversondmorais@gmail.com>
2025-06-09 00:47:12 -0300

    fix(new-task): corrigido impedimento de inserções de novas tarefas

--- src/app/todo/new-task/new-task.component.ts
+++ src/app/todo/new-task/new-task.component.ts
@@ -13,9 +13,8 @@ export class NewTaskComponent {
 
   constructor(private todoService: TodoService) { }
 
-  count = 0;
+
   addTask() {
-    if(this.count > 0) return
     const newTodo: Todo = {
       id: this.todoService.getTodoNewId(),
       title: this.newTaskTitle,
@@ -23,7 +22,6 @@ export class NewTaskComponent {
     };
 
     this.todoService.addTodo(newTodo);
-    this.newTaskTitle = '';
-    this.count++
+    this.newTaskTitle = '';                      
   }
 }
